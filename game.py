78cb04f250000479c55634123db4c018c23dacd5
Ola <ola.szalajko2@gmail.com>
2021-08-21 16:50:25 +0200

    add status

--- game.py
+++ game.py
@@ -16,24 +16,46 @@ board = RocketBoard(altitude, position, amountOfRockets)
 print(f'Ranking of the rockets: \n {board.get_ranking()}')
 print(f'Distance between rockets {distance1} and {distance2} is {board[distance1-1].get_distance(board[distance2-1])}')
 
+score = 0
+
 if board[maxDistance1-1].get_distance(board[maxDistance2-1]) == board.get_max_distance:
     print(f'Congratulation, distance between rockets {maxDistance1} and {maxDistance2} are the bigest')
+    score += 1
 else:
     print(f'Unfortunately rockets {maxDistance1} and {maxDistance2} are not farthest from each other')
 
+
 if board[minDistance1-1].get_distance(board[minDistance2-1]) == board.get_min_distance:
     print(f'Congratulation, distance between rockets {minDistance1} and {minDistance2} are least')
+    score += 1
 else:
     print(f'Unfortunately rockets {minDistance1} and {minDistance2} are not closest from each other')   
 
-if board.get_fastest_rocket() == FastestRocket:
+
+statusF = 0
+for i in range(len(board.get_fastest_rockets())):
+    if board.get_fastest_rockets()[i] == FastestRocket:
+        statusF += 1
+if statusF > 0:
     print(f'Congratulation, rocket {FastestRocket} is the fastest')
+    score += 1
 else:
     print(f'Unfortunately rocket {FastestRocket} is not the fastest')
 
-if board.get_slowest_rocket() == SlowestRocket:
+
+statusS = 0
+for i in range(len(board.get_slowest_rockets())):
+    if board.get_slowest_rockets()[i] == SlowestRocket:
+        statusS += 1
+if statusS > 0:
     print(f'Congratulation, rocket {SlowestRocket} is the slowest')
+    score += 1
 else:
     print(f'Unfortunately rocket {SlowestRocket} is not the slowest')
 
+
+if score > 0:
+    print(f'You win and score {score}!')
+else:
+    print('You lose :(')
 
